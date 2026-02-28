657f386d255985db7bc50b58651b2c835afbe870
AbdullahMRiad <abdullahmriad@gmail.com>
2026-03-01 00:57:15 +0300

    Split timer components to separate spans for easier selection

--- src/components/time.svelte
+++ src/components/time.svelte
@@ -13,14 +13,14 @@
     {#if duration === null}
         <span class="duration">00:00:00</span>
     {:else}
-        <span class="duration">
-            {duration.hours
-                ? duration.hours.toString().padStart(2, "0")
-                : "00"}:{duration.minutes
-                ? Math.abs(duration.minutes).toString().padStart(2, "0")
-                : "00"}:{duration.seconds
-                ? Math.abs(duration.seconds).toString().padStart(2, "0")
-                : "00"}
+        <span class="duration" dir="ltr">
+            {#each [{ c: "hours", v: duration.hours }, { c: "minutes", v: duration.minutes }, { c: "seconds", v: duration.seconds }] as { c, v }}
+                <span class={c}>
+                    {v
+                        ? v.toString().padStart(2, "0")
+                        : "00"}{#if c !== "seconds"}:{/if}
+                </span>
+            {/each}
         </span>
     {/if}
 </div>
