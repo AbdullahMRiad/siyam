5826b6003a054db028331486bf66a9d892631257
AbdullahMRiad <abdullahmriad@gmail.com>
2026-03-17 02:42:44 +0300

    Change time display to make all numbers have the same width mitigating the shifting when narrow numbers appear

--- src/components/time.svelte
+++ src/components/time.svelte
@@ -3,18 +3,38 @@
     import { status } from "../lib/status-manager.svelte";
 </script>
 
-<div style:font-family={settings.timeFontFamily}>
+<div
+    style:font-family={settings.timeFontFamily}
+    style:font-variant-numeric="tabular-nums">
     {#if status.timeUntilNextEvent === null}
         <span class="duration">00:00:00</span>
     {:else}
         <span class="duration" dir="ltr">
             {#each [{ c: "hours", v: status.timeUntilNextEvent.hours }, { c: "minutes", v: status.timeUntilNextEvent.minutes }, { c: "seconds", v: status.timeUntilNextEvent.seconds }] as { c, v }}
-                <span class={c}>
-                    {v
-                        ? v.toString().padStart(2, "0")
-                        : "00"}{#if c !== "seconds"}:{/if}
+                <span class={c} style:width="2ch">
+                    <span style:width="1ch">
+                        {v ? v.toString().padStart(2, "0").charAt(0) : "0"}
+                    </span>
+                    <span style:width="1ch">
+                        {v ? v.toString().padStart(2, "0").charAt(1) : "0"}
+                    </span>
                 </span>
+                {#if c !== "seconds"}
+                    <span class="colon">:</span>
+                {/if}
             {/each}
         </span>
     {/if}
 </div>
+
+<style>
+    .duration,
+    .duration > span {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .duration > span {
+        text-align: center;
+    }
+</style>
